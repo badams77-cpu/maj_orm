b9815932a48f440935c9fa591372f5734f954f33
Barry Adams <barry.david.adams@gmail.com>
2024-10-17 05:00:22 +0100

    master fix schema writer

--- src/main/java/com/majorana/maj_orm/SCHEME/WriteSchema.java
+++ src/main/java/com/majorana/maj_orm/SCHEME/WriteSchema.java
@@ -8,28 +8,30 @@ import com.majorana.maj_orm.ORM_ACCESS.EntityFinder;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.HashSet;
 import java.util.Set;
 
 public class WriteSchema {
 
     public static void main(String argv[]) {
-        if (argv.length != 3) {
+        if (argv.length != 4) {
             System.err.println("Usage: writeschema variant directory base_package");
             return;
         }
-
         DatabaseVariant variant = DatabaseVariant.valueOf(argv[0]);
         File dir = new File(argv[1]);
-        writeSchema(variant, dir,argv[2]);
+        writeSchema(variant, dir,argv[2], Boolean.parseBoolean(argv[3]));
     }
 
-    public static void writeSchema(DatabaseVariant variant, File dir, String bpackage){
+    public static void writeSchema(DatabaseVariant variant, File dir, String bpackage, boolean drop){
 
         EntityFinder ef = new EntityFinder();
         Set<Class> ec = ef.getEntitiesForPackagePrefix(bpackage);
+        System.err.println("Found "+ec.size()+" Domain classes in package "+bpackage);
         if (variant==DatabaseVariant.MYSQL){
 
             for(Class c : ec){
+                System.err.println("Schema for "+c.getCanonicalName());
                 try {
                     String name = c.getCanonicalName();
                     Object entity = c.newInstance();
@@ -40,12 +42,18 @@ public class WriteSchema {
                     }
                     File f = new File(dir, name + ".sql");
                     FileWriter fw = new FileWriter(f);
-                    fw.write("CREATE TABLE "+name+" IF NOT EXISTS (\n");
+                    Set<String> done = new HashSet();
+                    if (drop){
+                        fw.write("DROP TABLE IF EXISTS "+name+";\n\n");
+                    }
+                    fw.write("CREATE TABLE IF NOT EXISTS "+name+" (\n");
                     MajoranaRepositoryField id = mar.getIdField();
-                    fw.write(id.getDbColumn()+" int auto_increament primary key");
+                    fw.write(id.getDbColumn()+" int auto_increment primary key\n");
                     for(Object fieldO : mar.getRepoFields()){
                         MajoranaRepositoryField field = (MajoranaRepositoryField)  fieldO;
                         if (field.getDbColumn().equals(id.getDbColumn())){ continue; }
+                        if (done.contains(field.getDbColumn())){ continue; }
+
                         Class fieldType = field.getValueType();
                         if (fieldType.getCanonicalName().equals("java.lang.String")){
                             if (field.getVarcharSize()==0){
@@ -53,12 +61,17 @@ public class WriteSchema {
                             } else {
                                 fw.write(", "+ field.getDbColumn()+" varchar("+field.getVarcharSize() +")\n");
                             }
+                        } else {
+                            fw.write(", " + field.getDbColumn() + " " + mar.getSqlType(field) + "\n");
                         }
-                        fw.write(", "+field.getDbColumn()+" "+mar.getSqlType(field)+"\n");
+                        done.add(field.getDbColumn());
                     }
                     fw.write(");\n");
                     fw.close();
-                } catch (Exception e){}
+                    System.out.println("written schema to "+f);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
             }
 
         }
